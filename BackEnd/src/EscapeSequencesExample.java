94615052fc16d7c3dbf29bbc7790de95b4afec3e
iwstg <iwstg@naver.com>
2025-08-11 14:24:54 +0900

    Java 백엔드 기초

---
+++ BackEnd/src/EscapeSequencesExample.java
@@ -0,0 +1,9 @@
+public class EscapeSequencesExample {
+    public static void main(String[] args) {
+        // 여러 가지 이스케이프 시퀀스 사용
+        System.out.println("Hello\nWorld!");  // 줄바꿈
+        System.out.println("Hello\tWorld!");  // 탭
+        System.out.println("He said, \"Java is fun!\"");  // 큰 따옴표
+        System.out.println("Path: C:\\Program Files\\Java");  // 백슬래시
+    }
+}
---
+++ BackEnd/src/ForEx.java
@@ -0,0 +1,11 @@
+import org.w3c.dom.ls.LSOutput;
+
+public class ForEx {
+    public static void main(String[] args) {
+        int i = 0;
+        while( i++ <= 5){
+            System.out.println(i + " 번째");
+        }
+        System.out.println("i = " + i);
+    }
+}
