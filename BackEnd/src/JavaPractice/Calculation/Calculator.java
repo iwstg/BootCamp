9559c81d97146d88daaffbc6912cb070d8bd7e7f
iwstg <iwstg@naver.com>
2025-08-12 17:22:53 +0900

    java 실습 및 과제

--- BackEnd/src/JavaPractice/Calculation/Calculator.java
+++ BackEnd/src/JavaPractice/Calculation/Calculator.java
@@ -5,22 +5,22 @@ import java.util.Scanner;
 public class Calculator {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String opretor;
+        String operator;
         double num1, num2;
         System.out.println("과제 - 3");
         do {
             System.out.print("연산자를 입력하세요 (+, -, *, /) : ");
-            opretor = sc.nextLine();
-            if (!opretor.equals("+")&&!opretor.equals("-")&&!opretor.equals("*")&&!opretor.equals("/")) {
+            operator = sc.nextLine();
+            if (!operator.equals("+")&&!operator.equals("-")&&!operator.equals("*")&&!operator.equals("/")) {
                 System.out.println("연산자를 잘못입력하셨습니다. 다시 입력해주세요");
             }
 
-        }while(!opretor.matches("[+\\-*/]")); // + - * / 가 아닐때까지 반복
-        System.out.print("연산할 두 수를 공백을 두고 입력하세요 (현재 "+opretor+" 연산 ): ");
+        }while(!operator.matches("[+\\-*/]")); // + - * / 가 아닐때까지 반복
+        System.out.print("연산할 두 수를 공백을 두고 입력하세요 (현재 "+operator+" 연산 ): ");
         num1 = sc.nextDouble();
         num2 = sc.nextDouble();
 
-        switch (opretor) {
+        switch (operator) {
             case "+":
                 System.out.println(num1 + " + " + num2 + " = "+ (num1+num2));
                 break;
