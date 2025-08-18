2108cf69cc1c891317642520174ee6a286c7c615
iwstg <iwstg@naver.com>
2025-08-18 14:38:22 +0900

    250818 제네릭 과제

--- BackEnd/src/javapractice/generic/Main.java
+++ BackEnd/src/javapractice/generic/Main.java
@@ -1,4 +1,63 @@
 package javapractice.generic;
 
-public class GenericBox {
+import java.util.function.Function;
+
+class GenericBox<T>{
+    T value;
+
+    public GenericBox(T value) {
+        this.value = value;
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    public void setValue(T value) {
+        this.value = value;
+    }
+
+    public boolean isPresent(){
+        return value != null;
+    }
+
+    static <T> GenericBox<T> of(T value){
+        return new GenericBox<>(value);
+    }
+
+    static <T> void swap(GenericBox<T> a,GenericBox<T> b){
+        T Temp;
+        Temp = b.getValue();
+        b.setValue(a.getValue());
+        a.setValue(Temp);
+    }
+
+    public <R> GenericBox<R> map(Function<? super T, ? extends R> mapper) {
+        return new GenericBox<>(mapper.apply(value));
+    }
+
+
+}
+
+public class Main {
+    public static void main(String[] args) {
+        GenericBox<Integer> box1 = new GenericBox<>(50);    // 생성자 초기화
+        GenericBox<Integer> box2 = GenericBox.of(500);      // 메소드 초기화
+        GenericBox<String> box3 = GenericBox.of("Summer");
+
+        System.out.println(box1.isPresent());       // true
+        System.out.println(box1.getValue());        // 50
+        box1.setValue(100);
+        System.out.println(box1.getValue());        // 100
+
+        System.out.println(box2.getValue());        // 100
+        GenericBox.swap(box1,box2);                 // box1의 100이 box2로, box2의 500이 box1로
+        System.out.println("box 1: " + box1.getValue() + ", box 2: " + box2.getValue());
+
+//        GenericBox.swap(box2,box3);       // 자료형 불일치 오류
+
+        GenericBox<Double> doubox1 = box1.map(i -> i * 1.5);    // 500 * 1.5 값을 가지는 double 형태의 box 생성
+
+        System.out.println(doubox1.getValue());     // 750.0
+    }
 }
--- BackEnd/src/javapractice/rentcar/Main.java
+++ BackEnd/src/javapractice/rentcar/Main.java
@@ -1,4 +1,106 @@
 package javapractice.rentcar;
 
+interface Rentable{
+    void rent();
+}
+
+interface Returnable{
+    void returnVehicle();
+}
+
+abstract class Vehicle implements Rentable, Returnable{
+    protected String brand;
+    protected String model;
+
+    public Vehicle(String brand, String model) {
+        this.brand = brand;
+        this.model = model;
+    }
+
+    abstract int calculateRentalFee(int Rentdays);
+}
+
+
+class Car extends Vehicle implements Rentable, Returnable{
+
+    public Car(String brand, String model) {
+        super(brand, model);
+    }
+
+    @Override
+    public void rent() {
+        System.out.println(brand + "사 의 " + model + " 차량 대여 시작");
+    }
+
+    @Override
+    public void returnVehicle() {
+        System.out.println(brand + "사 의 " + model + " 차량 반납");
+    }
+
+    @Override
+    int calculateRentalFee(int days) {
+        return days * 50000;
+    }
+}
+
+class Truck extends Vehicle implements Rentable, Returnable{
+
+    public Truck(String brand, String model) {
+        super(brand, model);
+    }
+
+    @Override
+    public void rent() {
+        System.out.println(brand + "사 의 " + model + " 차량 대여 시작");
+    }
+
+    @Override
+    public void returnVehicle() {
+        System.out.println(brand + "사 의 " + model + " 차량 반납");
+    }
+
+    @Override
+    int calculateRentalFee(int days) {
+        return days * 80000;
+    }
+}
+
+class ElectricCar extends Vehicle implements Rentable, Returnable{
+
+    public ElectricCar(String brand, String model) {
+        super(brand, model);
+    }
+
+    @Override
+    public void rent() {
+        System.out.println(brand + "사 의 " + model + " 차량 대여 시작");
+    }
+
+    @Override
+    public void returnVehicle() {
+        System.out.println(brand + "사 의 " + model + " 차량 반납");
+    }
+
+    @Override
+    int calculateRentalFee(int days) {
+        System.out.println("전기차 충전비가 추가됩니다.");
+        return days * 70000;
+    }
+}
+
+
 public class Main {
+    public static void main(String[] args) {
+        Vehicle[] vehicles = {
+                new Car("Hyundai", "Sonata"),
+                new Truck("Kia", "Bongo"),
+                new ElectricCar("Tesla", "Model 3")
+        };
+
+        for(Vehicle v : vehicles){
+            v.rent(); // 대여 시작
+            v.returnVehicle(); // 반납 완료
+            System.out.println(v.calculateRentalFee(6)+ " 원"); // 6일 대여 시 가격
+        }
+    }
 }
